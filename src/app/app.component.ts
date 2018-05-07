1ab68e8a01dd8b4168f478db91c009c4c0209b8c
Fripley <filipecheverrya@gmail.com>
2018-05-07 17:12:39 -0300

    the first commit w/ content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,9 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  title = 'Mr House'
+  subtitle = 'Fallout New Vegas'
+  imgSrc = 'assets/img/mr_house.jpeg'
 }
